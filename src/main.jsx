a2dd010da562ebe1b114a9a68c4c243119d330d7
Nathan Sharma <nathansharma007@gmail.com>
2025-11-15 12:28:47 -0600

    more edits

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HashRouter, Routes, Route } from 'react-router-dom'; 
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
 import App from './App';
-import Register from './Register.jsx';
-import Info from './Info.jsx';
+import Register from './Register';
+import Info from './Info';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <HashRouter> 
-      <Routes> 
-        <Route path="/" element={<App />} /> 
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
         <Route path="/registration" element={<Register />} />
         <Route path="/info-and-about" element={<Info />} />
       </Routes>
